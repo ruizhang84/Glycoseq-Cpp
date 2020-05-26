3d6323b2988d280643d7039b1f57f231c49d2fc6
Yu Zhang <ruizhang84.mail@gmail.com>
2020-05-25 20:20:29 -0400

    Add the test

--- engine/glycan/builder_test.cpp
+++ engine/glycan/builder_test.cpp
@@ -1,6 +1,7 @@
 #define BOOST_TEST_MODULE BUilderTest
 #include <boost/test/unit_test.hpp>
 
+#include <iostream>
 #include "glycan_builder.h"
 
 
@@ -11,6 +12,17 @@ BOOST_AUTO_TEST_CASE( glycan_builder_test )
 {
     GlycanBuilder builder(2, 3, 1, 0, 0);
     builder.Build();
+    std::cout <<  builder.Isomer().Map().size() << std::endl;
+    for(auto& it :  builder.Isomer().Map()){
+        std::cout << it.first << std::endl;
+        for (auto& j: it.second)
+        {
+            std::cout << j << std::endl;
+        }
+        std::cout << std::endl;
+    }
+    BOOST_CHECK(builder.Isomer().Map().size() > 10);
+
 }
 
 }
